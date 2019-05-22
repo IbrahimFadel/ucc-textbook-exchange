8dd0442db6794103701c8c90c7485d7bc2a42d54
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2019-05-22 13:02:57 -0400

    Updates

---
+++ precache-manifest.c50b4e90bea502b142ab49f3227cd02f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1fd95545b0d1b6e8a7a5f811eefc14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a8613dc8c8e49990c0b",
+    "url": "/static/css/2.e7c12b49.chunk.css"
+  },
+  {
+    "revision": "ee8a5a841d8a16398061",
+    "url": "/static/css/main.a4d82059.chunk.css"
+  },
+  {
+    "revision": "6a8613dc8c8e49990c0b",
+    "url": "/static/js/2.d5dfa1b0.chunk.js"
+  },
+  {
+    "revision": "ee8a5a841d8a16398061",
+    "url": "/static/js/main.5ef4293b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9f309b3d47969ecac64a77a6c672594",
+    "url": "/static/media/NexaBold.c9f309b3.otf"
+  },
+  {
+    "revision": "12108809f49c49bcdc126dcecc938761",
+    "url": "/static/media/NexaLight.12108809.otf"
+  }
+]);
