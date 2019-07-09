b20cce5e50b6068d41bd42a386786bb0e75eac8a
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-07-09 15:31:27 -0700

    Profile now accessible

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,52 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "./firebase/firebase";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/Navbar.css";
 
+const PICTURE_SIZE = 50;
+
 export default class Navbar extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			user: null,
+			uid: null
+		};
+	}
+
+	componentDidMount() {
+		auth.onAuthStateChanged(user => {
+			if (user) {
+				this.setState(
+					{
+						user: user,
+						uid: user.uid
+					},
+					() => {}
+				);
+			} else {
+				this.setState({
+					user: null,
+					uid: null
+				});
+			}
+		});
+	}
+
 	burgerClicked() {
 		var links = document.querySelector(".nav-links");
 		console.log(links);
 		links.classList.toggle("links-active");
 	}
+
+	profileClicked = () => {
+		document
+			.getElementById("profile-dropdown")
+			.classList.toggle("dropdown-toggled");
+	};
 
 	render() {
 		return (
@@ -34,6 +71,27 @@ export default class Navbar extends Component {
 							<Link to="/login">Login/Signup</Link>
 						</li>
 					</ul>
+					{this.state.user ? (
+						<div id="profile-picture">
+							<div class="dropdown">
+								<img
+									src={
+										"https://api.adorable.io/avatars/" +
+										PICTURE_SIZE +
+										"/" +
+										this.state.uid
+									}
+									className="dropbtn"
+									onClick={this.profileClicked}
+								/>
+								<div class="dropdown-content">
+									<Link to="/profile">Profile</Link>
+								</div>
+							</div>
+						</div>
+					) : (
+						<div />
+					)}
 					<div className="burger" onClick={this.burgerClicked}>
 						<div className="line1" />
 						<div className="line2" />
