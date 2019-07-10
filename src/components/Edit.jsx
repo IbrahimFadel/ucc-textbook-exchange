dfb7d9df9025b46fc97bb1dac81af0ce23c6c2c7
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-07-10 09:47:09 -0700

    Editing done, currently unable to edit images

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -11,6 +11,7 @@ var data = undefined;
 
 var ogTitle;
 var ogDescription;
+var ogGrade;
 
 export default class Edit extends Component {
 	constructor() {
@@ -19,7 +20,8 @@ export default class Edit extends Component {
 		this.state = {
 			response: false,
 			title: undefined,
-			description: undefined
+			description: undefined,
+			grade: undefined
 		};
 	}
 
@@ -27,52 +29,61 @@ export default class Edit extends Component {
 		data = this.props.location.state;
 		ogTitle = data.title;
 		ogDescription = data.description;
+		ogGrade = data.grade;
 		this.setState({
 			title: ogTitle,
 			description: ogDescription,
+			grade: ogGrade,
 			response: true
 		});
-		console.log(data);
 	}
 
 	fieldChanged = field => {
-		if (field === "title") {
-			this.setState({
-				title: document.getElementById("title-input").value
-			});
-		} else if (field === "description") {
-			this.setState({
-				description: document.getElementById("description-textarea").value
-			});
+		switch (field) {
+			case "title":
+				this.setState({
+					title: document.getElementById("title-input").value
+				});
+			case "description":
+				this.setState({
+					description: document.getElementById("description-textarea").value
+				});
+			case "grade":
+				this.setState({
+					grade: document.getElementById("grade-select").value
+				});
 		}
 	};
 
 	cancel = field => {
-		if (field === "title") {
-			document.getElementById("title-input").value = ogTitle;
-			this.setState({
-				title: ogTitle
-			});
-		} else if (field === "description") {
-			document.getElementById("description-textarea").value = ogDescription;
-			this.setState({
-				description: ogDescription
-			});
+		switch (field) {
+			case "title":
+				document.getElementById("title-input").value = ogTitle;
+				this.setState({
+					title: ogTitle
+				});
+			case "description":
+				document.getElementById("description-textarea").value = ogDescription;
+				this.setState({
+					description: ogDescription
+				});
+			case "grade":
+				document.getElementById("grade-select").value = ogGrade;
+				this.setState({
+					grade: ogGrade
+				});
 		}
 	};
 
 	save = field => {
-		this.saveField(field);
-	};
-
-	saveField = field => {
-		const grade =
+		const gradeName =
 			data.grade === "Diploma Program" ? "diplomaprogram" : data.grade;
-		const path = `/listings/${grade}/${data.listingKey}`;
+		const path = `/listings/${gradeName}/${data.listingKey}`;
 
 		const description =
-			field === "description" ? this.state.description : data.description;
-		const title = field === "title" ? this.state.title : data.title;
+			field === "description" ? this.state.description : ogDescription;
+		const title = field === "title" ? this.state.title : ogTitle;
+		const grade = field === "grade" ? this.state.grade : ogGrade;
 
 		firebase
 			.database()
@@ -80,22 +91,57 @@ export default class Edit extends Component {
 			.set({
 				description: description,
 				email: data.email,
-				grade: data.grade,
+				grade: grade,
 				imageName: data.imageName,
 				sold: data.sold,
 				title: title,
 				uid: data.uid
 			})
 			.then(() => {
-				ogTitle = this.state.title;
-				this.setState(
-					{
-						title: ogTitle
-					},
-					() => {
-						Swal.fire("Success!", "Your title has been changed!", "success");
-					}
-				);
+				switch (field) {
+					case "title":
+						ogTitle = this.state.title;
+						this.setState(
+							{
+								title: ogTitle
+							},
+							() => {
+								Swal.fire(
+									"Success!",
+									"Your title has been changed!",
+									"success"
+								);
+							}
+						);
+					case "description":
+						ogDescription = this.state.description;
+						this.setState(
+							{
+								description: ogDescription
+							},
+							() => {
+								Swal.fire(
+									"Success!",
+									"Your description has been changed!",
+									"success"
+								);
+							}
+						);
+					case "grade":
+						ogGrade = this.state.grade;
+						this.setState(
+							{
+								grade: ogGrade
+							},
+							() => {
+								Swal.fire(
+									"Success!",
+									"Your grade has been changed!",
+									"success"
+								);
+							}
+						);
+				}
 			});
 	};
 
@@ -188,6 +234,54 @@ export default class Edit extends Component {
 									className="btn btn-dark formButton"
 									type="button"
 									onClick={() => this.save("description")}
+								>
+									Save
+								</button>
+							</div>
+						)}
+						<br />
+						<label>Grade</label>
+						<br />
+						<select
+							value={this.state.grade}
+							id="grade-select"
+							onChange={() => this.fieldChanged("grade")}
+						>
+							<option value="Diploma Program">Diploma Program</option>
+							<option value="grade10">Grade 10</option>
+							<option value="grade9">Grade 9</option>
+							<option value="grade8">Grade 8</option>
+						</select>
+						{this.state.grade === ogGrade ? (
+							<div>
+								<button
+									className="btn btn-dark formButton"
+									type="button"
+									disabled
+								>
+									Cancel
+								</button>
+								<button
+									className="btn btn-dark formButton"
+									type="button"
+									disabled
+								>
+									Save
+								</button>
+							</div>
+						) : (
+							<div>
+								<button
+									className="btn btn-dark formButton"
+									type="button"
+									onClick={() => this.cancel("grade")}
+								>
+									Cancel
+								</button>
+								<button
+									className="btn btn-dark formButton"
+									type="button"
+									onClick={() => this.save("grade")}
 								>
 									Save
 								</button>
